a82a5c49fb20e4a488d3b616ad35e95bcf702d52
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-03-25 14:03:59 -0500

    refactor(Empleado): move models to independent files in models package

---
+++ src/main/java/models/Empleado.java
@@ -0,0 +1,35 @@
+package models;
+import java.util.concurrent.ThreadLocalRandom;
+
+//Java 8 class:
+public class Empleado {
+
+    //Random id for user.
+    private long id = ThreadLocalRandom.current().nextLong(1, Long.MAX_VALUE);
+
+    //Features of each user (Primitives).
+    private byte age;
+    private short office;
+    private double salary;
+    private char gender;
+    private float bonus;
+    private boolean isActive;
+    private int languageQuantity;
+
+    //In Java, String is an object and isn't part of the primitives.
+    private String fullName;
+
+    //Constructor.
+    public Empleado(byte age, short office, double salary, char gender, float bonus,
+                    boolean isActive, int languageQuantity, String fullName){
+        this.age = age;
+        this.office = office;
+        this.salary = salary;
+        this.gender = gender;
+        this.bonus = bonus;
+        this.isActive = isActive;
+        this.languageQuantity = languageQuantity;
+        this.fullName = fullName;
+    }
+
+}
