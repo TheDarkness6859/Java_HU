32f610c6f852a29c1476acc531134a51f5edfb8e
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-03-26 23:02:59 -0500

    refactor(BusinessRecord): remove unnecessary parameter

--- src/main/java/models/BussisnesRecord.java
+++ src/main/java/models/BussisnesRecord.java
@@ -2,5 +2,5 @@ package models;
 
 //Java 17/21 Record
 public record BussisnesRecord(String name, long NIT, int foundationAge,
-                            double salary, boolean isWorking){
+                             boolean isWorking){
 }
