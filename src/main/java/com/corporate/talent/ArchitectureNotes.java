1eab46de53c6e0157dc42daf1a6d5c27101e1e61
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-03-26 22:50:31 -0500

    feat(ArchitectureNotes): laboratory tests

--- src/main/java/com/corporate/talent/ArchitectureNotes.java
+++ src/main/java/com/corporate/talent/ArchitectureNotes.java
@@ -64,8 +64,27 @@ public class ArchitectureNotes {
         System.out.println(textContent);
 
         ArchitectureNotes app = new ArchitectureNotes();
-        app.credentials();
 
+        System.out.println("laboratory tests:");
+
+        String emptyName = null;
+
+        try {
+            System.out.println(emptyName.length());
+        }catch (NullPointerException e){
+            System.out.println("Error diagnostic:");
+            System.out.println("Message:" + e.getMessage());
+        }
+
+        BussisnesRecord data = new BussisnesRecord("Riwi", 800123456, 2000, true);
+        System.out.println("Registered business: " + data.name());
+
+        Employable empA = new Employable((byte)20, (short)10, 1000.0, 'M', 0.1f, true, 2, "Test", 80);
+        Employable empB = new Employable((byte)20, (short)10, 1000.0, 'M', 0.1f, true, 2, "Test", 80);
+
+        System.out.println("\n¿empA == empB is:? " + (empA == empB));
+
+        app.credentials();
     }
 
     public void credentials(){
