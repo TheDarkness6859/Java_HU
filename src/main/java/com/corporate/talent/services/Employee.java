3c55b507c90bd35a6708368c674a5b0ffa36e947
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-06 20:10:34 -0500

    feat(Employee): add method getEmployee

--- src/main/java/com/corporate/talent/services/Employee.java
+++ src/main/java/com/corporate/talent/services/Employee.java
@@ -10,5 +10,20 @@ public class Employee {
     private ArrayList<Employable> employees = new ArrayList<>();
     private HashMap<Long, Employable> idEmployee = new HashMap<>();
 
+    public void getAllEmployees() {
+
+        if (idEmployee.isEmpty()){
+            System.out.println("No employees registered yet");
+            return;
+        }
+
+        idEmployee.forEach((id, emp) -> {
+
+            System.out.println("ID:" + id + "| Name:" + emp.getFullName());
+
+        });
+
+    }
+
 }
 
