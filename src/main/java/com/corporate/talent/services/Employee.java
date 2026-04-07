9787acc331cf7f00f29f3fdd6d6b2b1097490452
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-06 19:44:16 -0500

    feat(Employee): implement ArrayList and HashMap for storage

---
+++ src/main/java/com/corporate/talent/services/Employee.java
@@ -0,0 +1,14 @@
+package com.corporate.talent.services;
+
+import com.corporate.talent.models.Employable;
+import com.corporate.talent.ui.EmployeeUI;
+
+import java.util.*;
+
+public class Employee {
+
+    private ArrayList<Employable> employees = new ArrayList<>();
+    private HashMap<Long, Employable> idEmployee = new HashMap<>();
+
+}
+
