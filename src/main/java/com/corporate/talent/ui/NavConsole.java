2c3288f35bbc108a3049b43a3188331bc402f455
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-07 11:04:25 -0500

    refactor(NavConsole): update navegation flow for improved UX/UI

--- src/main/java/com/corporate/talent/ui/NavConsole.java
+++ src/main/java/com/corporate/talent/ui/NavConsole.java
@@ -1,6 +1,7 @@
 package com.corporate.talent.ui;
 
 import com.corporate.talent.models.Employable;
+import com.corporate.talent.services.Employee;
 import com.corporate.talent.services.Performance;
 import com.corporate.talent.services.Salary;
 
@@ -10,59 +11,55 @@ import java.util.Scanner;
 public class NavConsole {
 
     //Instances
-    EmployeeUI cre = new EmployeeUI();
+    Employee  employee = new Employee();
+    EmployeeUI interaction = new EmployeeUI(employee);
     Salary sal = new Salary();
     Performance per = new Performance();
 
     public void navConsole() {
 
-        String console = """
-                -----------------------------
-                |          MENU             |
-                -----------------------------
-                | 1) Create a employee      |
-                | 2) Watch employee salary  |
-                | 3) Salary category        |
-                | 4) Employee notes         |
-                | 5) Exit                   |
-                -----------------------------
-                """
-                ;
-
         Scanner dec = new Scanner(System.in);
         Employable e = null;
         int desc;
+        long id;
 
         do {
             try {
-                System.out.println(console);
+                ConsoleBanners.mainConsole();
                 System.out.println("Which is your decision?:");
                 desc = dec.nextInt();
                 dec.nextLine();
 
                 switch (desc) {
                     case 1:
-                        e = cre.credentials();
-                        System.out.println("Employee created!");
+                        interaction.employeeDec();
                         break;
                     case 2:
+                        id = interaction.search();
+                        e = employee.findEmployee(id);
+                        break;
+                    case 3:
                         if (e != null){
                             sal.salary(e);
                         }else {
                             System.out.println("You don't have a employee.");
                         }
                         break;
-                    case 3:
-                        sal.obtainSalaryCategory(e);
-                        break;
                     case 4:
                         if (e != null){
-                            per.EmployeeData(e);
+                            sal.obtainSalaryCategory(e);
                         }else {
-                            System.out.println("You don't have a employee.");
+                            System.out.println("Please, search for an employee first (Option 2).");
                         }
                         break;
                     case 5:
+                        if (e != null){
+                            per.EmployeeData(e);
+                        }else {
+                            System.out.println("Please, search for an employee first (Option 2).");
+                        }
+                        break;
+                    case 6:
                         break;
                 }
             }catch (InputMismatchException err){
@@ -71,6 +68,6 @@ public class NavConsole {
                 desc = 0;
             }
 
-        }while (desc != 5);
+        }while (desc != 6);
     }
 }
