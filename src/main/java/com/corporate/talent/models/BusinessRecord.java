5bfff6640cacc813cb65b483a85e9dedd987ae21
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-08 13:25:07 -0500

    feat(BusinessRecord): add id, technology list, and branch mapping

--- src/main/java/com/corporate/talent/models/BusinessRecord.java
+++ src/main/java/com/corporate/talent/models/BusinessRecord.java
@@ -1,6 +1,9 @@
 package com.corporate.talent.models;
 
+import java.util.*;
+
 //Java 17/21 Record
-public record BusinessRecord(String name, long NIT, int foundationAge,
-                             boolean isWorking){
+public record BusinessRecord(long id, String name, long NIT, int foundationAge,
+                             boolean isWorking, List<String> technologies, Map<String, Integer> Sedes){
+
 }
