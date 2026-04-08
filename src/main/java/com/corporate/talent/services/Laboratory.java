fdf68bd420b6217cded729e2313040ac14dce1f6
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-08 13:51:08 -0500

    refactor(Laboratory): delete unnecessary code

--- src/main/java/com/corporate/talent/services/Laboratory.java
+++ src/main/java/com/corporate/talent/services/Laboratory.java
@@ -1,6 +1,5 @@
 package com.corporate.talent.services;
 
-import com.corporate.talent.models.BusinessRecord;
 import com.corporate.talent.models.Employable;
 
 public class Laboratory {
@@ -16,9 +15,6 @@ public class Laboratory {
             System.out.println("Error diagnostic:");
             System.out.println("Message:" + e.getMessage());
         }
-
-        BusinessRecord data = new BusinessRecord("Riwi", 800123456, 2000, true);
-        System.out.println("Registered business: " + data.name());
 
         Employable empA = new Employable((byte)20, (short)10, 1000.0, 'M', 0.1f, true, 2, "Test", 80);
         Employable empB = new Employable((byte)20, (short)10, 1000.0, 'M', 0.1f, true, 2, "Test", 80);
