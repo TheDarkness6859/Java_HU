7a6a7a97ba3ddcbe1e570e295f68b06c0a873dae
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-28 09:02:31 -0500

    feat(Main): add instances to constructor injection

--- src/main/java/com/corporate/talent/Main.java
+++ src/main/java/com/corporate/talent/Main.java
@@ -1,17 +1,37 @@
 package com.corporate.talent;
 
-import com.corporate.talent.services.Laboratory;
+import com.corporate.talent.services.*;
+import com.corporate.talent.views.BusinessView;
 import com.corporate.talent.ui.ConsoleBanners;
-import com.corporate.talent.ui.NavConsole;
+import com.corporate.talent.views.EmployeeView;
+import com.corporate.talent.utils.ScannerInput;
+import com.corporate.talent.views.NavConsoleView;
+
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //Instances
-        Laboratory lab = new Laboratory();
-        NavConsole console = new NavConsole();
+        //Class:
+        Employee emp = new Employee();
+
+        //Utils:
+        Scanner scanner = new Scanner(System.in);
+        ScannerInput input = new ScannerInput(scanner);
+
+        //Services:
+        Salary sal = new Salary();
+        Performance perf = new Performance();
+        FactoryData factory = new FactoryData();
+        Business business = new Business();
+
+        //Views:
+        BusinessView businessview = new BusinessView(input, factory, business);
+        EmployeeView employeeView = new EmployeeView(emp , input);
+        NavConsoleView console = new NavConsoleView(employeeView, businessview, emp, sal, perf, input);
+
 
         ConsoleBanners.banner();
         console.navConsole();
