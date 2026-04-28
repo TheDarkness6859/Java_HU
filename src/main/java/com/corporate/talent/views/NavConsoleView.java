4e3571e10c7f47892a3b6bab72f1f57020fdbc24
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-28 08:17:26 -0500

    feat(NavConsoleView): add utility class ScannerInput to easier manage of Scanner

--- src/main/java/com/corporate/talent/views/NavConsoleView.java
+++ src/main/java/com/corporate/talent/views/NavConsoleView.java
@@ -3,9 +3,7 @@ package com.corporate.talent.views;
 import com.corporate.talent.models.Employable;
 import com.corporate.talent.services.*;
 import com.corporate.talent.ui.*;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import com.corporate.talent.utils.ScannerInput;
 
 public class NavConsoleView {
 
@@ -14,72 +12,72 @@ public class NavConsoleView {
     private final Employee employee;
     private final Salary sal;
     private final Performance per;
+    private final ScannerInput input;
 
-    public NavConsoleView(EmployeeUI intEmp, BusinessUI intBus, Employee employee, Salary sal, Performance per){
+    public NavConsoleView(EmployeeUI intEmp,
+                          BusinessUI intBus,
+                          Employee employee,
+                          Salary sal,
+                          Performance per,
+                          ScannerInput input){
 
         this.intEmp = intEmp;
         this.intBus = intBus;
         this.employee = employee;
         this.sal = sal;
         this.per = per;
+        this.input = input;
 
     }
 
     public void navConsole() {
 
-        Scanner dec = new Scanner(System.in);
         Employable e = null;
-        int desc;
+        int desc = 0;
         long id;
 
         do {
-            try {
-                ConsoleBanners.mainConsole();
-                System.out.println("Which is your decision?:");
-                desc = dec.nextInt();
-                dec.nextLine();
 
-                switch (desc) {
-                    case 1:
-                        intEmp.employeeDec();
-                        break;
-                    case 2:
-                        intBus.businessDec();
-                        break;
-                    case 3:
-                        employee.getAllEmployees();
-                        id = intEmp.search();
-                        e = employee.findEmployee(id);
-                        break;
-                    case 4:
-                        if (e != null){
-                            sal.salary(e);
-                        }else {
-                            System.out.println("Please, search for an employee first (Option 3).");
-                        }
-                        break;
-                    case 5:
-                        if (e != null){
-                            sal.obtainSalaryCategory(e);
-                        }else {
-                            System.out.println("Please, search for an employee first (Option 3).");
-                        }
-                        break;
-                    case 6:
-                        if (e != null){
-                            per.EmployeeData(e);
-                        }else {
-                            System.out.println("Please, search for an employee first (Option 3).");
-                        }
-                        break;
-                    case 7:
-                        break;
-                }
-            }catch (InputMismatchException err){
-                System.err.println("Please enter a number.");
-                dec.nextLine();
-                desc = 0;
+            ConsoleBanners.mainConsole();
+            desc = input.readInt("Which is your decision?: ");
+
+            switch (desc) {
+                case 1:
+                    intEmp.employeeDec();
+                    break;
+                case 2:
+                    intBus.businessDec();
+                    break;
+                case 3:
+                    employee.getAllEmployees();
+                    id = intEmp.search();
+                    e = employee.findEmployee(id);
+                    break;
+                case 4:
+                    if (e != null){
+                        sal.salary(e);
+                    }else {
+                        System.out.println("Please, search for an employee first (Option 3).");
+                    }
+                    break;
+                case 5:
+                    if (e != null){
+                        sal.obtainSalaryCategory(e);
+                    }else {
+                        System.out.println("Please, search for an employee first (Option 3).");
+                    }
+                    break;
+                case 6:
+                    if (e != null){
+                        per.EmployeeData(e);
+                    }else {
+                        System.out.println("Please, search for an employee first (Option 3).");
+                    }
+                    break;
+                case 7:
+                    break;
             }
+
 
         }while (desc != 7);
     }
